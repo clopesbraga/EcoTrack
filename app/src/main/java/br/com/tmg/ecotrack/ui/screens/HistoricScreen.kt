09909d90c6d7187f156e5c14f783084f0821950b
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-07-10 20:18:04 -0300

    feature(HistoricScreen): criada e configurada inicialmente tela do histórico.

---
+++ app/src/main/java/br/com/tmg/ecotrack/ui/screens/HistoricScreen.kt
@@ -0,0 +1,16 @@
+package br.com.tmg.ecotrack.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun HistoricScreen() {
+
+    Column{
+        Text(text="Lista historico de viagens")
+    }
+
+}
