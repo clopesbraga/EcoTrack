a783529a4acbd3495d9a8de7f1f8e1e0c25e757c
Cleiton Lopes Braga <clopesbraga@gmail.com>
2024-07-15 00:49:12 -0300

    feature(Captura-imagem): configuracao tela lista de imagens.

--- app/src/main/java/br/com/tmg/ecotrack/ui/screens/HistoricScreen.kt
+++ app/src/main/java/br/com/tmg/ecotrack/ui/screens/HistoricScreen.kt
@@ -1,16 +1,95 @@
 package br.com.tmg.ecotrack.ui.screens
 
+import android.net.Uri
+import androidx.core.net.toUri
+import android.provider.MediaStore
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+
+import androidx.compose.ui.unit.dp
+import br.com.tmg.ecotrack.model.database.ImageItemModel
+import br.com.tmg.ecotrack.viewmodel.ImagesViewModel
+import org.koin.androidx.compose.koinViewModel
 
 
 @Composable
 fun HistoricScreen() {
 
-    Column{
-        Text(text="Lista historico de viagens")
-    }
+    val viewModel = koinViewModel<ImagesViewModel>()
+    val context = LocalContext.current
+    val imagelist = viewModel.listImages()
 
+    Column {
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(imagelist) { imageitem ->
+                val uri = Uri.parse(imageitem.imageUri)
+                val bitmap = try {
+                    MediaStore.Images.Media.getBitmap(
+                        context.contentResolver,
+                        uri.toString().toUri()
+                    )
+                } catch (e: Exception) {
+                    Log.d("BITMAP ERROR", e.toString())
+                    null
+                }
+                bitmap?.let {
+                    Card(modifier = Modifier.padding(16.dp)){
+                        Row(modifier =Modifier
+                            .fillMaxSize(),
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Image(
+                                bitmap = it.asImageBitmap(),
+                                contentDescription = null,
+                                contentScale = ContentScale.Crop,
+                                modifier = Modifier
+                                    .padding(16.dp)
+                                    .size(100.dp)
+                                    .clip(CircleShape)
+
+                            )
+                            Column(modifier = Modifier
+                                .padding(16.dp),
+                                horizontalAlignment = Alignment.Start,
+                                verticalArrangement = Arrangement.Center
+                            ) {
+                                Text(text = "Local:${imageitem.local}")
+                                Text(text = "Data: ${imageitem.data}")
+                                Text(text = "Hora: ${imageitem.hora}")
+                            }
+                        }
+                    }
+
+
+                }
+
+
+            }
+        }
+    }
 }
--- app/src/main/java/br/com/tmg/ecotrack/ui/screens/MapScreen.kt
+++ app/src/main/java/br/com/tmg/ecotrack/ui/screens/MapScreen.kt
@@ -1,23 +1,46 @@
 package br.com.tmg.ecotrack.ui.screens
 
-import androidx.compose.foundation.layout.Arrangement
+import android.Manifest
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
+import br.com.tmg.ecotrack.viewmodel.ImagesViewModel
+import org.koin.androidx.compose.koinViewModel
+
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import kotlinx.coroutines.launch
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Objects
 
 
 @Composable
@@ -27,16 +50,27 @@ fun MapScreenPreview() {
     MapScreen()
 }
 
-
-val singapore = LatLng(1.35, 103.87)
-
-
 @Composable
 fun MapScreen() {
 
+    val viewModel = koinViewModel<ImagesViewModel>()
+
+    var currentLocation by remember { mutableStateOf<LatLng?>(null) }
+
     val cameraPositionState = rememberCameraPositionState {
-        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+        position = CameraPosition
+            .fromLatLngZoom(currentLocation?:LatLng(0.0, 0.0),5f)
     }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(Unit) {
+
+        coroutineScope.launch {
+            val location = viewModel.getCurrentLocation()
+            currentLocation = location?.let {LatLng(it.latitude, it.longitude) }
+        }
+    }
+
     Box(
         Modifier.fillMaxSize(),
     ) {
@@ -45,14 +79,87 @@ fun MapScreen() {
 
             cameraPositionState = cameraPositionState,
 
-        ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+            ) {
+            currentLocation?.let {
+                Marker(
+                    state = MarkerState(position = it),
+                    title = "Your Location",
+                    snippet = "You are here"
+                )
+            }
         }
 
     }
+    TakePicture(viewModel)
 }
 
+
+@Composable
+private fun TakePicture(viewModel: ImagesViewModel) {
+
+
+    val context = LocalContext.current
+    val file = context.createImageFile()
+    val uri = FileProvider.getUriForFile(
+        Objects.requireNonNull(context),
+        context.packageName + ".provider", file
+    )
+
+    var capturedImageUri by remember {
+        mutableStateOf<Uri>(Uri.EMPTY)
+    }
+
+    var cameraLauncher =
+        rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) {
+
+            capturedImageUri = uri
+
+        }
+    var permissionLauncher = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            Toast.makeText(context, "Permissão garantida", Toast.LENGTH_SHORT).show()
+            cameraLauncher.launch(uri)
+        } else {
+            Toast.makeText(context, "Permissão negada", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    Box(contentAlignment = Alignment.CenterEnd) {
+        ExtendedFloatingActionButton(
+            onClick = {
+                val permissionchekResult = ContextCompat.checkSelfPermission(
+                    context, Manifest.permission.CAMERA
+                )
+
+                if (permissionchekResult == PackageManager.PERMISSION_GRANTED) {
+                    cameraLauncher.launch(uri)
+                } else {
+                    permissionLauncher.launch(Manifest.permission.CAMERA)
+                }
+
+
+            }) {
+            Icon(Icons.Filled.Edit, "Tirar Foto")
+            Text(text = "Tirar Foto")
+        }
+
+
+        if (capturedImageUri.path?.isNotEmpty() == true) {
+            viewModel.saveImage(capturedImageUri)
+        }
+
+    }
+
+
+}
+
+private fun Context.createImageFile(): File {
+
+    val timestamp = SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(Date())
+    val imageFileName = "EcoTrack_${timestamp}"
+    val image = File.createTempFile(imageFileName, ".jpg", externalCacheDir)
+
+    return image
+}
